023fd0ec6bcfaa4a62597609fbbb81da1cbb6339
arun-kumar-rout <routarunkumar31@gmail.com>
2023-11-01 12:55:00 +0530

    added router and google authentication

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,14 +1,46 @@
-import React from 'react'
-import Image from 'next/image'
-import {FcGoogle} from 'react-icons/fc'
+"use client"
+import React, {useEffect} from 'react';
+import Image from 'next/image';
+import {FcGoogle} from 'react-icons/fc';
+// pushing user to another page
+import { useRouter } from 'next/navigation';
 
-const page = () => {
+// importing firebase google authentication
+import {getAuth, signInWithPopup, GoogleAuthProvider} from 'firebase/auth';
+import {app} from "../../../firebase.config";
+
+import { userAccessToken } from '../../../utils/fetchLocalUserDetails';
+
+
+
+const Login = () => {
+  const firebaseAuth = getAuth(app);
+  const provider = new GoogleAuthProvider();
+  const router = useRouter();
+  const signIn = async () => {
+    // adding popup functionality by google firebase
+    const {user} = await signInWithPopup(firebaseAuth, provider);
+    const {refreshToken, providerData} = user;
+    
+    // storing providerData and refreshToken in local browser
+    localStorage.setItem("user",JSON.stringify(providerData));
+    localStorage.setItem("accessToken", JSON.stringify(refreshToken));
+
+    // login user should be pushed to home page
+    router.push('/')
+    
+  }
+  // user having firebase access token can not access signin page again
+  useEffect(() => {
+    const accessToken = userAccessToken();
+    if(accessToken) return router.push("/");
+  }, [router]);
   return (
     <div className='w-screen h-screen flex justify-center items-center bg-white relative'>
       <Image className='absolute top-0 left-0 h-screen w-screen object-cover' src="/assets/images/sunset.jpg" alt='sunset' width={1200} height={600}/>
       <div className='absolute top-0 left-0 right-0 bottom-0 bg-black bg-opacity-50'></div>
 
-      <div className='z-10 flex justify-center items-center bg-white bg-opacity-60 pe-4 p-2 rounded-full cursor-pointer hover:shadow-md hover:bg-opacity-100 duration-150 ease-in-out'>
+      <div onClick={signIn} className='z-10 flex justify-center items-center bg-white bg-opacity-60 pe-4 p-2 rounded-full cursor-pointer hover:shadow-md hover:bg-opacity-100 duration-150 ease-in-out'>
         <FcGoogle fontSize={30}/>
         <p className='text-lg font-semibold ml-4'>Sign in with Google</p>
       </div>
@@ -16,4 +48,4 @@ const page = () => {
   )
 }
 
-export default page
+export default Login
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,49 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
+import {
+  userAccessToken,
+  fetchUserDetails,
+} from "../../utils/fetchLocalUserDetails";
+import { useRouter } from "next/navigation";
+
+import { IoLogOut } from "react-icons/io5";
 
 export default function Home() {
+  const router = useRouter();
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    const accessToken = userAccessToken();
+    if (!accessToken) return router.push("/login");
+
+    const [userInfo] = fetchUserDetails();
+    console.log(userInfo);
+    setUser(userInfo);
+  }, [router]);
+  const logOut = () => {
+    localStorage.clear();
+    router.push("/login");
+  }
   return (
     <div className="w-screen h-screen bg-slate-100 flex justify-center items-center">
-      Index
+      <div className="w-1/3 h-auto p-4 bg-white shadow-md rounded-md justify-start items-center relative">
+        <IoLogOut onClick={logOut}
+          fontSize={25}
+          className="absolute top-8 right-3 cursor-pointer text-gray-600"
+        />
+        {user != undefined ? (
+          <div className="flex">
+            <Image className="rounded-xl object-cover" src={user.photoURL} alt="profile" width={50} height={50} />
+            <div className="p-2">
+              <h1>{user.displayName}</h1>
+              <h1>{user.email}</h1>
+            </div>
+          </div>
+        ) : (
+          <h1>Loading</h1>
+        )}
+      </div>
     </div>
-  )
+  );
 }
